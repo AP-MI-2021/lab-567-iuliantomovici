4aa7124a1b78622e4331567763866fdbfc66f478
Tomovici Iulian <iulian.tomovici@stud.ubbcluj.ro>
2021-10-27 07:34:35 +0300

    lab5

--- Tests/test_domain.py
+++ Tests/test_domain.py
@@ -1,4 +1,6 @@
-from Domain.rezervari import creeaza_rezervare,get_ID,get_nume,get_pret,get_clasa,get_checkin_facut
+from Domain.rezervari import *
+
+
 def test_creeaza_rezervare():
     rezervare= creeaza_rezervare(1,'Popescu Ion','business',1200,'nu')
 
@@ -7,4 +9,11 @@ def test_creeaza_rezervare():
     assert get_clasa(rezervare) == 'business'
     assert get_pret(rezervare) == 1200
     assert get_checkin_facut(rezervare) =='nu'
-
+    set_nume(rezervare,'ionut')
+    assert get_nume(rezervare)=='ionut'
+    set_pret(rezervare,123)
+    assert get_pret(rezervare)==123
+    set_clasa(rezervare,'economy')
+    set_checkin_facut(rezervare,'da')
+    assert get_clasa(rezervare)=='economy'
+    assert get_checkin_facut(rezervare)=='da'
